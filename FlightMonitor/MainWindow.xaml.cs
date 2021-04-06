f27e4dc585edd21f4f188b084323314f3d361b02
Dvir <59093573+DvirAsaf@users.noreply.github.com>
2021-04-06 19:04:00 +0300

    resolve conflicts2

--- FlightMonitor/MainWindow.xaml.cs
+++ FlightMonitor/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Threading;
 namespace FlightMonitor
 {
     /// <summary>
@@ -21,27 +20,33 @@ namespace FlightMonitor
     /// </summary>
     public partial class MainWindow : Window
     {
-        MyFlightgearMonitorModel model;
         public MainWindow()
         {
             InitializeComponent();
             main_window.Show();
-            model = new MyFlightgearMonitorModel(new MyTelnetClient());
-            ControlBar1.HookVM(model);
-            // ControlBar control_bar = new ControlBar();
-            //control_bar.HookVM(model);
+            ControlBar control_bar = new ControlBar();
             Thread thr = new Thread(letsBegin);
             thr.Start();
             
-           /* main_window.Show();
-            Control control_bar = new Control();*/
         }
 
         private void letsBegin()
-        { 
-            model.connect("localhost", 5400);
-            model.start();
-            model.disconnect();
+        {
+            MyFlightgearMonitorModel letsStart;
+            letsStart = new MyFlightgearMonitorModel(new MyTelnetClient());
+            letsStart.connect("localhost", 5400);
+            letsStart.start();
+            letsStart.disconnect();
+        }
+
+        private void Fly_default_click(object sender, RoutedEventArgs e)
+        {
+            //VM.execute();
+        }
+
+        private void ControlBar_Loaded(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 
